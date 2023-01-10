96b36920e8f95ae02e8451746301e983990b2e19
Aevum Decessus <aevum@decess.us>
2023-01-09 20:32:54 -0500

    Revo Hemera is NOT inverted like microswiss

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1666,7 +1666,8 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR true
+// AevumEdit Revo Hemera Direct Drive is not inverted like MicroSwiss Direct Drive
+#define INVERT_E0_DIR false
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
